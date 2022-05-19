146fb46a4fc46f653f91dddca838285ba992db72
Anurag Singh <78840945+anuragsingh17ai@users.noreply.github.com>
2022-05-19 08:29:33 +0000

    Update the_number_of_position.cpp

--- the_number_of_position.cpp
+++ the_number_of_position.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 int main()
 {
-	int n,a,b;
+	int n,a,b,cnt=0;
 	cin>>n>>a>>b;
-	cout<<n-min(a,b);
+	for(int i=1;i<=n;i++)
+		if(i-1>=a && n-i<=b)
+			cnt++;
+	cout<<cnt<<endl;
 	return 0;
 }
